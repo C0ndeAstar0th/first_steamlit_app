72bc05e4c9e6dee815cb7a4ef1dbf16b7bf1e891
Freddy Montañez <36642580+C0ndeAstar0th@users.noreply.github.com>
2022-08-16 14:25:02 -0500

    Add emojis and a new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title(' My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Frtee-Range Egg')
+streamlit.header(' Breakfast Menu')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Frtee-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
