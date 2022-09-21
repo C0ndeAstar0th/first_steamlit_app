28516ce86fc4971f0b166acd8db6a8e4b0e30db9
Freddy Montañez <36642580+C0ndeAstar0th@users.noreply.github.com>
2022-09-21 11:51:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,7 @@ try:
 except URLError as e:
  streamlit.error()
 
- #Snowflake part
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+ #Snowflake part =my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 streamlit.header("The fruit load list contains:")
 
 fruit_choice_snowflake = streamlit.text_input('What fruit would you like information about?')
